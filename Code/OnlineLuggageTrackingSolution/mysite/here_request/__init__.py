151b9d1f86da391f4387d607ba43fc9ae6766b7f
AntonLiutov <aliutov@kse.org.ua>
2019-11-17 10:24:53 +0300

    Update __init__.py

--- Code/OnlineLuggageTrackingSolution/mysite/here_request/__init__.py
+++ Code/OnlineLuggageTrackingSolution/mysite/here_request/__init__.py
@@ -4,7 +4,5 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '02d9899c28cdb5e02e51d942cbd8e848' 
-# app.config['SQLACLHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-# db = SQLAlchemy(app)
 
 from here_request import routes
